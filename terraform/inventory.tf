00e090ad17123766eba2a0448ab32babf3063647
Marco Auenhammer <marco.auenhammer@gmail.com>
2026-04-23 18:02:26 +0200

    add target & attacker inventory entries

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -1,13 +1,17 @@
 resource "local_file" "ansible_inventory" {
-  filename = "${path.module}/../ansible/inventory/hosts.yml"
+  filename        = "${path.module}/../ansible/inventory/hosts.yml"
   file_permission = "0644"
 
   content = yamlencode({
     all = {
       hosts = {
-        (var.baseline_vm_name) = {
-          ansible_host = module.baseline.primary_ipv4
-          proxmox_vmid = module.baseline.vm_id
+        attacker = {
+          ansible_host = module.attacker.primary_ipv4
+          proxmox_vmid = module.attacker.vm_id
+        }
+        target = {
+          ansible_host = split("/", var.target_lab_ipv4)[0]
+          proxmox_vmid = module.target.vm_id
         }
       }
     }

a3c18260f4b35aea28a1fcdbe9e89491214e86d0
Marco Auenhammer <marco.auenhammer@gmail.com>
2026-04-23 18:24:05 +0200

    add inventory template

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -2,18 +2,10 @@ resource "local_file" "ansible_inventory" {
   filename        = "${path.module}/../ansible/inventory/hosts.yml"
   file_permission = "0644"
 
-  content = yamlencode({
-    all = {
-      hosts = {
-        attacker = {
-          ansible_host = module.attacker.primary_ipv4
-          proxmox_vmid = module.attacker.vm_id
-        }
-        target = {
-          ansible_host = split("/", var.target_lab_ipv4)[0]
-          proxmox_vmid = module.target.vm_id
-        }
-      }
-    }
+  content = templatefile("${path.module}/templates/inventory.yml.tftpl", {
+    attacker_mgmt_ip = module.attacker.primary_ipv4
+    attacker_vm_id   = module.attacker.vm_id
+    target_lab_ip    = module.target.primary_ipv4
+    target_vm_id     = module.target.vm_id
   })
 }
